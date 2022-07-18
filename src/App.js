b91d443cb1379492f7e33f17b4dedf9e7c231c39
Adam West <Adamwest023@gmail.com>
2022-07-18 16:18:03 -0500

    added hooks to retrieve and set weather  and forecast api data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { Weather_Api_Url, Weather_Api_Key, Forecast_Api_Key } from './api'
+import { useState } from 'react';
 
 function App() {
 
-  const handleOnSearchChange=(searchData) => {
-    console.log(searchData);
+  //create hooks
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(`${Weather_Api_Url}/weather?lat=${lat}&lon=${lon}&appid=${Weather_Api_Key}`);
+
+    const weatherForecastFetch = fetch(`${Weather_Api_Url}/forecast?lat=${lat}&lon=${lon}&appid=${Forecast_Api_Key}`);
+
+    Promise.all([currentWeatherFetch, weatherForecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        //creates array with the information from our api fetch and we use the spread operator to combine the search information
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+
+      })
+      //catch error 
+      .catch((err) => console.log(err));
+
   }
+
+  console.log(currentWeather); 
+  console.log(forecast); 
   return (
     <div className="container">
-     <Search onSearchChange={handleOnSearchChange} />
-     <CurrentWeather  />
-      
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+
+
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -8,6 +8,14 @@ export const geoApiOptions = {
 
 export const Geo_Api_Url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+//current weather api 
+export const Weather_Api_Url = "https://api.openweathermap.org/data/2.5"
+
+export const Weather_Api_Key = "6d8a07a5eb7eca0fdd645a3bccfce2ef"
+
+//5 day forcast 
+
+export const Forecast_Api_Key = "71c8690292d6042d2e5133616017daf0 ";
 
 
     
